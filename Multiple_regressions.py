735388b64765ed95c3694054a88935a64349282e
Tomomi Tanaka <tomomit@gmail.com>
2022-09-13 13:13:24 +0900

    Create Multiple_regressions.py
    multiple regressions

---
+++ Multiple_regressions.py
@@ -0,0 +1,10 @@
+# Example 3.1 in Woodridge "Introductory Econometrics: A Modern Approach"
+
+import wooldridge as woo
+import statsmodels.formula.api as smf
+
+gpa1 = woo.dataWoo('gpa1')
+
+reg = smf.ols(formula='colGPA ~ hsGPA + ACT', data=gpa1)
+results = reg.fit()
+print(f'results.summary(): \n{results.summary()}\n')
